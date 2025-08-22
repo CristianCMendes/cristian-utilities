20b79aba48969e103c806a5782d8f58add276879
CristianCM Lider Atacadista <cristian.mendes@lideratacadista.com.br>
2025-08-21 23:57:55 -0300

    Refatorado `asInt` para simplificar a lógica e eliminar logs desnecessários
    - Removidos `console.log` do código.
    - Simplificada a atribuição de `v` com uso direto de `removeNonNumeric`.

--- src/assets/utils/numbers.ts
+++ src/assets/utils/numbers.ts
@@ -2,12 +2,8 @@ import {removeNonNumeric} from "@utils/string.ts";
 
 export function asInt(n: string | number | undefined): number {
     if (n == undefined) return 0
-    console.log(n)
     if (typeof n == 'number') return parseInt(n.toFixed(0))
-    let v = parseInt(removeNonNumeric(n))
-    console.log(v)
-
-    if (isNaN(v)) v = parseInt(removeNonNumeric(n.toString()))
+    const v = parseInt(removeNonNumeric(n.toString()))
 
     return isNaN(v) ? 0 : v
 }
